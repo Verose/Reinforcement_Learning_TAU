1e53a1b373c7f4a694348029904f4d4161352b08
wolkomor <wolkomor@gmail.com>
2019-03-16 18:51:37 +0200

    final

--- Exercise3.py
+++ Exercise3.py
@@ -16,20 +16,23 @@ def find_most_probable_word(word_size):
     f = np.full((num_letters, word_size+1), np.inf)
 
     # end of the word - the consecutive must be '-'
-    #f[num_letters-1, word_size] = 1
-    f[:, word_size] = cost_matrix[:, 3]
+    f[num_letters-1, word_size] = 1
+    #f[3, word_size+1] = cost_matrix[:, 3]
 
     for i in reversed(range(word_size)):
-        if i!=0: #if not the first letter
-            for letter, c in enumerate(cost_matrix):
-                f[letter, i] = np.min(c*f[:, i + 1].transpose())
-        else:  # if is is the first letter - it must be letter b (in index 0)
-            f[0, i] = np.min(cost_matrix[0, :]*f[:, i + 1].transpose())
+        if i!=0 and i!=word_size-1: #if not the first letter and not the last (the last letter must be -)
+            for letter in range(num_letters-1):
+                f[letter, i] = np.min(cost_matrix[letter, 0:(num_letters-1)]*f[0:(num_letters-1), i + 1].transpose())
+        elif i==word_size-1: #if it is the last letter it must be "-"
+            for letter in range(num_letters-1):
+                f[letter, i] = np.min(cost_matrix[letter, num_letters-1] * f[:, i + 1].transpose())
+        elif i == 0:  # if is is the first letter of the word - it must be letter b (index 0)
+            f[0, i] = np.min(cost_matrix[0, 0:(num_letters-1)]*f[0:(num_letters-1), i + 1].transpose())
 
-    probable_word = [letters_simb[np.argmin(f[:, 0])]]
-    for i in range(1, word_size):
-        first_word = np.argmin(f[:, i])
-        probable_word.append(letters_simb[first_word])
+    probable_word = []
+    for lett_in_word in range(word_size+1):
+        word_index = np.argmin(f[:, lett_in_word])
+        probable_word.append(letters_simb[word_index])
 
     return probable_word
 
