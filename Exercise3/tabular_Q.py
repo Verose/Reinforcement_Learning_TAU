1b88a386fa5d291c289f735d23682c1448074f15
eytanc <eytanc@gmail.com>
2019-04-20 19:59:30 +0300

    Tabular_Q all zeros bug fix
    The issue was that we were choosing the same action over and over again before learned the first good action.
    (in line 31, the argmax() chooses the first instance of the maximum. So, if we haven't learned anything yet (meaning that the maximum is 0) then we should always sample randomly

--- Exercise3/tabular_Q.py
+++ Exercise3/tabular_Q.py
@@ -14,6 +14,7 @@ num_episodes = 2000
 # create lists to contain total rewards and steps per episode
 # jList = [] TODO: From Eytan - Don't know what this if for
 rList = []
+np.random.seed(123) # TODO: We can comment this out in the submission
 for i in range(num_episodes):
     # Reset environment and get first new observation
     s = env.reset()
@@ -26,7 +27,7 @@ for i in range(num_episodes):
         # TODO: Implement Q-Learning
         # 1. Choose an action by greedily (with noise) picking from Q table
         e_t = 1 / (j ** 0.9)
-        if np.random.uniform(0,1) <= 1-e_t:
+        if np.random.uniform(0,1) <= 1-e_t and Q[s].max() != 0:
             a = Q[s].argmax()
         else:
             a = env.action_space.sample()
