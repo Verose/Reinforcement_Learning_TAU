5e6afefc6493f5b64bac2e94c2280032152a69ba
eytanc <eytanc@gmail.com>
2019-05-21 11:10:04 +0300

    BatchNorm

--- Project/dqn/dqn_model.py
+++ Project/dqn/dqn_model.py
@@ -25,6 +25,34 @@ class DQN(nn.Module):
         x = F.relu(self.fc4(x.view(x.size(0), -1)))
         return self.fc5(x)
 
+class DQN_bn(nn.Module):
+    def __init__(self, in_channels=4, num_actions=18):
+        """
+        Initialize a deep Q-learning network as described in
+        https://storage.googleapis.com/deepmind-data/assets/papers/DeepMindNature14236Paper.pdf
+        Arguments:
+            in_channels: number of channel of input.
+                i.e The number of most recent frames stacked together as describe in the paper
+            num_actions: number of action-value to output, one-to-one correspondence to action in game.
+        """
+        super(DQN_bn, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.Batchnorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.Batchnorm2d(64)
+        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        self.fc5 = nn.Linear(512, num_actions)
+
+    def forward(self, x):
+        x = self.bn1(F.relu(self.conv1(x)))
+        x = self.bn2(F.relu(self.conv2(x)))
+        x = self.bn3(F.relu(self.conv3(x)))
+        x = F.relu(self.fc4(x.view(x.size(0), -1)))
+        return self.fc5(x)
+
+
 class DQN_RAM(nn.Module):
     def __init__(self, in_features=4, num_actions=18):
         """
