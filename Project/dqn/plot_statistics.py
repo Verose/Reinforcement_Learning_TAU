9bb1415e10ed077c5c6ef99e35fa13f7580a0471
veredz <verose@live.com>
2019-05-23 07:58:10 +0300

    Project: load and plot several pickles

--- Project/dqn/plot_statistics.py
+++ Project/dqn/plot_statistics.py
@@ -1,6 +1,7 @@
 """
     This file is copied/apdated from https://github.com/berkeleydeeprlcourse/homework/tree/master/hw3
 """
+import argparse
 import os
 import platform
 import sys
@@ -27,21 +28,29 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-with open('statistics.pkl', 'rb') as f:
-    saved_state = pickle.load(f)
+parser = argparse.ArgumentParser(description='DQN args')
+parser.add_argument('--pkls', required=True, nargs='+', help='pickle files to load')
+parser.add_argument('--labels', required=True, nargs='+', help='names for pickles for legend')
+
+args = parser.parse_args()
 
 
 plt.clf()
 plt.xlabel('Timesteps')
 plt.ylabel('Mean Reward (past 100 episodes)')
-num_items = len(saved_state.stats["mean_episode_rewards"])
 
-mean_episodes = [item[1] for item in saved_state.stats["mean_episode_rewards"]]
-best_mean = [item[1] for item in saved_state.stats["best_mean_episode_rewards"]]
-t_mean = [item[0] for item in saved_state.stats["mean_episode_rewards"]]
-t_best = [item[0] for item in saved_state.stats["best_mean_episode_rewards"]]
+for pkl, label in args.pkls, args.labels:
+    with open(pkl, 'rb') as f:
+        saved_state = pickle.load(f)
 
-plt.plot(t_mean, mean_episodes, label='mean reward')
-plt.plot(t_best, best_mean, label='best mean rewards')
+    num_items = len(saved_state.stats["mean_episode_rewards"])
+
+    mean_episodes = [item[1] for item in saved_state.stats["mean_episode_rewards"]]
+    best_mean = [item[1] for item in saved_state.stats["best_mean_episode_rewards"]]
+    t_mean = [item[0] for item in saved_state.stats["mean_episode_rewards"]]
+    t_best = [item[0] for item in saved_state.stats["best_mean_episode_rewards"]]
+
+    plt.plot(t_mean, mean_episodes, label='mean reward {}'.format(label))
+    plt.plot(t_best, best_mean, label='best mean rewards {}'.format(label))
 plt.legend()
 plt.savefig('statistics.png')
